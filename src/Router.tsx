f74c35dce2281e34fc45f4e938f52e67c0c51ff7
DoubleJD <jd009@naver.com>
2023-03-09 23:18:35 +0900

    gh-page 이용시 버그 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,7 +4,7 @@ import Coins from "./routes/Coins";
 
 function Router() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/react-app">
       <Switch>
         <Route path="/:coinId">
           <Coin />
